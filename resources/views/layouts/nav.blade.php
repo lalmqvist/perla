8a3edf4820a7e03a23d43f81870471cda7c54e0e
Linda Almqvist <linda.almqvist@chas.se>
2018-01-23 14:21:31 +0100

    added a link in the nav for accessing the cart page

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -6,24 +6,12 @@
     
     <a class="navbar-brand" href="/"><img style="width: 130px" src="../img/logo/logo_grey.png" alt=""></a>
    
+    <a href="/cart"><button class="btn my-2 my-sm-0 icons" type="button" id="dropdown-cart">
+        b
+      </button></a>
+    {{--  @include('layouts.cart')  --}}
+    @include('layouts.search')
 
-
-          {{--  Search field and toggler - start  --}}
-          <button class="btn my-2 my-sm-0 icons navbar-toggler" type="button" data-toggle="collapse" data-target="#collapseSearchField" aria-expanded="false" aria-controls="collapseSearchField" id="dropdown-search">
-            s
-          </button>
-
-        <div class="collapse" id="collapseSearchField">
-          <div class="card card-body dropdown-search">
-              <form class="form-inline mt-2 mt-md-0">
-                  
-                <input class="form-control mr-sm-2" type="text" placeholder="Sök..." aria-label="Search">
-                <button class="btn my-2 my-sm-0 icons" type="submit">n</button>
-
-              </form>          
-            </div>
-        </div>
-        {{--  Search field and toggler - end   --}}
 
     
       <button class="navbar-toggler icons" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
---
+++ resources/views/layouts/search.blade.php
@@ -0,0 +1,14 @@
+{{--  Search field and toggler - start  --}}
+<button class="btn my-2 my-sm-0 icons navbar-toggler" type="button" data-toggle="collapse" data-target="#collapseSearchField" aria-expanded="false" aria-controls="collapseSearchField" id="dropdown-search">
+      s
+    </button>
+  <div class="collapse" id="collapseSearchField">
+    <div class="card card-body dropdown-search">
+        <form class="form-inline mt-2 mt-md-0">
+            
+          <input class="form-control mr-sm-2" type="text" placeholder="Sök..." aria-label="Search">
+          <button class="btn my-2 my-sm-0 icons" type="submit">n</button>
+        </form>          
+      </div>
+  </div>
+  {{--  Search field and toggler - end   --}}
